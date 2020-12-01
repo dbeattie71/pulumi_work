ff4895d4bc513339cab7dcba74fb027f54bc447c
Mitch Gerdisch <mitchell.gerdisch@jesslex.com>
2020-12-01 08:37:46 -0600

    variations on apply theam

--- apply-cs/MyStack.cs
+++ apply-cs/MyStack.cs
@@ -7,16 +7,34 @@ class MyStack : Stack
     public MyStack()
     {
         var config = new Pulumi.Config();
-        var rgName = config.GetSecret("rgName"); 
-
-        // Create an Azure Resource Group
-        var resourceGroupName = rgName.Apply(rgn => {
-            var rg = new ResourceGroup(rgn, new ResourceGroupArgs
-            {
-                Name = rgn,
-            });
-            return rg.Name;
+        var rgName = config.RequireSecret("rgName"); 
+        
+        // Variations on the theme
+        
+        // Nicest answer
+        var rg = new ResourceGroup("my-rg", new ResourceGroupArgs
+        {
+            Name = Output.Format($"{rgName}"),
         });
+    
+        // Using Output.Tuple approach with a dummy variable, goo
+        //var rg = new ResourceGroup("my-rg", new ResourceGroupArgs
+        // {
+        //  Name = Output.Tuple<string, string>(rgName, goo).Apply(t =>
+        //  {
+        //     (string rgn, string g) = t;
+        //     return $"{rgn}";
+        //  }),
+        //});
+
+        // Resource creation in the Apply()
+        //var resourceGroupName = rgName.Apply(rgn => {
+        //    var rg = new ResourceGroup(rgn, new ResourceGroupArgs
+        //    {
+        //        Name = rgn,
+        //    });
+        //    return rg.Name;
+        //});
 
         var storageAccount = new Account("storage", new AccountArgs
         {
