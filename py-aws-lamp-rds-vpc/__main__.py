fcc97b348fb396c41a211b0d346ea13ffa0c5d19
MitchellGerdisch <mitch@pulumi.com>
2021-01-26 15:38:02 -0600

    good working version

--- py-aws-lamp-rds-vpc/__main__.py
+++ py-aws-lamp-rds-vpc/__main__.py
@@ -13,8 +13,8 @@ import frontend
 
 # Get config data
 config = pulumi.Config()
-project = config.get("project") or "wp-demo"
-db_name = config.get("db_name") or "wpdb"
+service_name = config.get("service_name") or "lamp-demo"
+db_name = config.get("db_name") or "lampdb"
 db_user = config.get("db_user") or "admin"
 db_password = config.get_secret("db_password") or "__makeapassword__"
 if db_password == "__makeapassword__":
@@ -22,18 +22,17 @@ if db_password == "__makeapassword__":
         length=16,
         special=True,
         override_special="_%@",
-        opts=pulumi.ResourceOptions(additional_secret_outputs=['result']))
+        )
     db_password = password.result
 
-name_base = "mitch"
 # Create an AWS VPC and subnets, etc
-network = network.Vpc(name_base, network.VpcArgs())
+network = network.Vpc(service_name, network.VpcArgs())
 subnet_ids = []
 for subnet in network.subnets:
     subnet_ids.append(subnet.id)
 
 # Create RDS instance
-db = database.Rds(name_base, database.RdsArgs(
+db = database.Rds(service_name, database.RdsArgs(
     db_name=db_name,
     db_user=db_user,
     db_password=db_password,
@@ -42,9 +41,7 @@ db = database.Rds(name_base, database.RdsArgs(
     security_group_ids=[network.rds_security_group.id]
 ))
 
-#fe = Output.all(db.rds.address, db.rds.name, db.rds.username, db.rds.passwordsql_server.name, database.name) \
-# .apply(lambda args: f"Server=tcp:{args[0]}.database.windows.net;initial catalog={args[1]}...")
-fe = frontend.WebService(name_base, frontend.WebServiceArgs(
+fe = frontend.WebService(service_name, frontend.WebServiceArgs(
     db_host=db.rds.address,
     db_port="3306",
     db_name=db.rds.name,
