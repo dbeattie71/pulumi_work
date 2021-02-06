171a24ccee34e95bf09ea9672c98baf01c64c9db
MitchellGerdisch <mitch@pulumi.com>
2021-02-06 14:37:22 -0600

    storing stuff

--- ts-aws-api-lambda-eventbridge-lambda-dynamodb/lambda-eventbridge-dyndb/index.ts
+++ ts-aws-api-lambda-eventbridge-lambda-dynamodb/lambda-eventbridge-dyndb/index.ts
@@ -1,23 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-const AWS = require("aws-sdk");
 
-const awsConfig = new pulumi.Config("aws");
-const region = awsConfig.get("region") || "us-east-1"
+const stack = pulumi.getStack();
+const project = pulumi.getProject();
 
 const config = new pulumi.Config();
 const nameBase = config.get("nameBase") || "mitch-new"
 const existingApiGwId = config.require("existingApiGwId")
 const existingApiGwName = config.require("existingApiGwName")
+const eventBridgeName = config.get("eventBridgeName") || `${project}-${stack}`
 
-// Import an existing API gateway
-const apiGw = new aws.apigatewayv2.Api(existingApiGwName, {
-  name: existingApiGwName,
-  protocolType: "HTTP",
-}, {import: existingApiGwId})
+
+/////// API GATEWAY -> LAMBDA PLUMBING
+// Find existing API gateway
+const apiGw = aws.apigatewayv2.Api.get(existingApiGwName, existingApiGwId)
 
 // Build Lambda #1 which receives requests from the imported API GW
-// TODO: lambda-1 component resource
 const lambdaRole = new aws.iam.Role(`${nameBase}-lambdarole`, {
   assumeRolePolicy: {
     Version: "2012-10-17",
@@ -43,6 +41,7 @@ const rolepolicyattachment = new aws.iam.RolePolicyAttachment(`${nameBase}-lambd
 // Create the Lambda to execute
 const lambda = new aws.lambda.Function(`${nameBase}-lambdaFunction`, {
   code: new pulumi.asset.AssetArchive({
+    "infra_info.js": new pulumi.asset.StringAsset(`module.exports.params={eventBridgeName: "${eventBridgeName}"}`),
     ".": new pulumi.asset.FileArchive("./lambda1-app"),
   }),
   runtime: "nodejs12.x",
@@ -51,26 +50,43 @@ const lambda = new aws.lambda.Function(`${nameBase}-lambdaFunction`, {
 });
 
 // Give API Gateway permissions to invoke the Lambda
-const lambdapermission = new aws.lambda.Permission(`${nameBase}-lambdaPermission`, {
+const lambdaPermission = new aws.lambda.Permission(`${nameBase}-lambdaPermission`, {
   action: "lambda:InvokeFunction",
   principal: "apigateway.amazonaws.com",
   function: lambda,
+  sourceArn: pulumi.interpolate`${apiGw.executionArn}/*/*`,
 });
 
-// Add Lambda #1 to the API GW
-// Set up route
-if (!pulumi.runtime.isDryRun()) {
-  const apiGwConfig = pulumi.all([apiGw.id]).apply(([apiGwId]) => {
-    AWS.config.update({region:region});
-    const client = new aws.sdk.ApiGatewayV2();
-    const params = {
-      ApiId: apiGwId,
-      RouteKey: "GET /",
-    }
-    client.getRoute
-    client.createRoute(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-    });
-  });
-}
+const integration = new aws.apigatewayv2.Integration(`${nameBase}-lambdaIntegration`, {
+  apiId: apiGw.id,
+  integrationType: "AWS_PROXY",
+  integrationUri: lambda.arn,
+  integrationMethod: "POST",
+  payloadFormatVersion: "2.0",
+  passthroughBehavior: "WHEN_NO_MATCH",
+});
+
+const route = new aws.apigatewayv2.Route(`${nameBase}-apiRoute`, {
+  apiId: apiGw.id,
+  routeKey: "GET /demo",
+  target: pulumi.interpolate`integrations/${integration.id}`,
+});
+
+const stage = new aws.apigatewayv2.Stage(`${nameBase}-apiStage`, {
+  apiId: apiGw.id,
+  name: stack,
+  routeSettings: [
+    {
+      routeKey: route.routeKey,
+      throttlingBurstLimit: 5000,
+      throttlingRateLimit: 10000,
+    },
+  ],
+  autoDeploy: true,
+});
+///// END OF API GATEWAY -> LAMBDA 1 PLUMBING
+
+///// EVENT BRIDGE SET UP
+
+
+export const apiUrl = pulumi.interpolate`${stage.invokeUrl}/demo`
--- ts-aws-api-lambda-eventbridge-lambda-dynamodb/lambda-eventbridge-dyndb/lambda1-app/index.js
+++ ts-aws-api-lambda-eventbridge-lambda-dynamodb/lambda-eventbridge-dyndb/lambda1-app/index.js
@@ -1,7 +1,8 @@
 exports.handler =  async function(event, context) {
   console.log("\n*** EVENT RECEIVED ***: \n" + JSON.stringify(event, null, 2))
+  const { params } = require('./infra_info.js')
   return {
       statusCode: 200,
-      body: "Hello, Pulumi!"
+      body: `${params.eventBridgeName} --- Hello, Pulumi!`
   };
 }
