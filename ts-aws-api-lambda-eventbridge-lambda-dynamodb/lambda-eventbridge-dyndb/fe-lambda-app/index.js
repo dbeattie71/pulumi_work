4bf0d8bdfc7b282659e1fc62f7376b01c41e3c7f
MitchellGerdisch <mitch@pulumi.com>
2021-02-07 13:07:58 -0600

    lovely

--- ts-aws-api-lambda-eventbridge-lambda-dynamodb/lambda-eventbridge-dyndb/fe-lambda-app/index.js
+++ ts-aws-api-lambda-eventbridge-lambda-dynamodb/lambda-eventbridge-dyndb/fe-lambda-app/index.js
@@ -1,34 +1,33 @@
+// Get the info about the infrastructure that was just created/updated by Pulumi
+const { infraInfo } = require('./infra_info')
+const eventBusName = infraInfo.eventBusName
+const eventSource = infraInfo.eventSource
+
+// Setup for passing stuff to the bus
 const AWS = require('aws-sdk')
 AWS.config.region = process.env.AWS_REGION || 'us-east-1'
 const eventbridge = new AWS.EventBridge()
 
+// Handle requests
+// This is a simple function that handles GET requests and 
+// creates an event for EventBridge that contains a timestamp and any query parameters.
 exports.handler = async (event, context) => {
-  // process API Calls ....
-  // TBD 
-
-  // Get the info about the infrastructure that was just created/updated by Pulumi
-  const { infraInfo } = require('./infra_info')
-  const eventBusName = infraInfo.eventBusName
-  const eventSource = infraInfo.eventSource
-
-  // And now create the event...
-
-  // parameters set up
   const timestamp = Date.now()
+
+  const eventDetailsStatic = {timestamp: timestamp}
+  const eventDetails = {...eventDetailsStatic, ...event.queryStringParameters}
+
+  // parameters for EventBus sdk call 
   const params = {
     Entries: [ 
     {
       // Event envelope fields
-      Source: eventSource,
-      EventBusName: eventBusName,
+      Source: eventSource, // provided by Pulumi code at deployment
+      EventBusName: eventBusName, // provided by Pulumi code at deployment
       DetailType: 'transaction',
       Time: timestamp,
       // Main event body
-      Detail: JSON.stringify({
-        timestamp: timestamp,
-        action: 'something-new-happened',
-        result: 'it-was-also-good',
-      })
+      Detail: JSON.stringify(eventDetails)
     },
   ]}
 
@@ -42,6 +41,6 @@ exports.handler = async (event, context) => {
 
   return {
       statusCode: 200,
-      body: JSON.stringify(params)
+      body: JSON.stringify(params, null, 4)
   };
 }
